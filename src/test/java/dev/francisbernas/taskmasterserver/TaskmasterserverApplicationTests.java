eb28a5823ce667a2cefc4cdcaf6214ef0e9c6d5e
francisjosephironman <francis.3.6.joseph@gmail.com>
2024-03-23 23:22:08 +0800

    initial commit

---
+++ src/test/java/dev/francisbernas/taskmasterserver/TaskmasterserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.francisbernas.taskmasterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmasterserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
